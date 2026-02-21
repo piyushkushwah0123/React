64e9a8c34c7d824fbcda1d2e7427db997e2c48c4
Piyush kushwah <piyushkushwah002@gmail.com>
2026-02-21 12:15:34 +0530

    Add files via upload

---
+++ Child1.jsx
@@ -0,0 +1,3 @@
+import react from 'react'
+import Child1 from './Child1'
+
---
+++ Child3.jsx
@@ -0,0 +1,9 @@
+import react from 'react'
+
+const Child3 = (props) => {
+    return{
+        <div>
+        <Child3 name= props.name 
+        </div>
+    }
+}
